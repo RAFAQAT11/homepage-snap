5f3a6fb25454a2d7770c9675eafe33880580e5a9
rafaqat11 <kashijhony@gmail.com>
2024-09-18 19:15:06 +0500

    update: chnage the link & remove the text

--- packages/snap/src/components/Search.tsx
+++ packages/snap/src/components/Search.tsx
@@ -1,4 +1,4 @@
-import { Box, Text, Input, Link } from '@metamask/snaps-sdk/jsx';
+import { Box, Input, Link } from '@metamask/snaps-sdk/jsx';
 import { SnapComponent } from '@metamask/snaps-sdk/jsx';
 
 export type SearchProps = {
@@ -9,10 +9,7 @@ export const Search: SnapComponent<SearchProps> = ({ domain = '' }) => {
   return (
     <Box direction="horizontal" alignment="center">
       <Input name="example-input" placeholder="Enter something..." />
-      <Box direction="vertical" alignment="center">
-        <Text>{''}</Text>
-        <Link href={`https://3ns-dev.surge.sh/?q=${domain}`}>Search</Link>
-      </Box>
+      <Link href={`https://3ns-v1.surge.sh/?q=${domain}`}>Search</Link>
     </Box>
   );
 };
